e60756ca138bb5bd65f46b9732bfc6c3757b5792
ayush <ayush-09>
2020-03-22 09:39:50 +0530

    display the board function is created

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -1,1 +1,7 @@
-def display_board(board):
+def display_board(board):  # display the board
+    print('|', end="")
+    print(board[1] + board[2] + board[3] + '|')
+    print('|', end="")
+    print(board[4] + board[5] + board[6] + '|')
+    print('|', end="")
+    print(board[7] + board[8] + board[9] + '|')
