c6bace33461e51221b1c7b649d85ad04e5f144fa
ayush <ayush-09>
2020-03-22 09:42:20 +0530

    create the function of player_input for player chance

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -5,3 +5,6 @@ def display_board(board):  # display the board
     print(board[4] + board[5] + board[6] + '|')
     print('|', end="")
     print(board[7] + board[8] + board[9] + '|')
+
+
+def player_input():
