d8ccc8e1fc4ca821d9bc47f8c9c372cd95a3b6c8
ayush <ayush-09>
2020-03-28 15:08:37 +0530

    create function named replay to play the game again

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -58,3 +58,6 @@ def player_choice(board):
     while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         position = int(input('Choose a position: (1-9)'))
     return position
+
+
+def replay():
